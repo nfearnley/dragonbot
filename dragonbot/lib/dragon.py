5ea7ff321a4965ca1a321ef0a7c19d7437c378e8
DigiDuncan <digiduncan@gmail.com>
2021-05-19 02:58:01 -0400

    more attributes!

--- dragonbot/lib/dragon.py
+++ dragonbot/lib/dragon.py
@@ -1,19 +1,31 @@
+from decimal import Decimal
 from typing import List, Literal, Optional, Tuple
 
 GenderPreset = Literal['f', 'h', 'm', 'n', 'p', '?']
+StandardModifiers = Literal['+++!', '+++', '++', '+', '', '-', '--', '---', '---!']
 
 
-class Length:
-    def __init__(self) -> None:
+class Attribute:
+    letter = None
+
+    def __init__(self):
         pass
 
+    @classmethod
+    def parse(cls, code: str):
+        if not code.startswith(cls.letter):
+            raise ValueError(f"{cls.__name__} attribute doesn't start with {cls.letter}.")
 
-class Gender:
-    def __init__(self, preset_gender: GenderPreset, custom_gender: str, variable: List[str],
-                 transition: Tuple[str, str], discrepancy: Tuple[str, str]) -> None:
+    def __str__(self) -> str:
+        raise NotImplementedError
 
-        if sum(a is not None for a in (preset_gender, custom_gender, variable, transition, discrepancy)) != 1:
-            raise TypeError("Didn't pass one type of gender in.")
+
+class Gender(Attribute):
+    letter = "G"
+
+    def __init__(self, preset_gender: Optional[GenderPreset] = None, custom_gender: Optional[str] = None,
+                 variable: Optional[List[str]] = None, transition: Optional[Tuple[str, str]] = None,
+                 discrepancy: Optional[Tuple[str, str]] = None):
 
         self.preset_gender = preset_gender
         self.custom_gender = custom_gender
@@ -21,17 +33,80 @@ class Gender:
         self.transition = transition
         self.discrepancy = discrepancy
 
+        self.options = (self.preset_gender, self.custom_gender, self.variable, self.transition, self.discrepancy)
+
+        if sum(a is not None for a in self.options) != 1:
+            raise TypeError("Didn't pass one type of gender in.")
+
+    @property
+    def active_type(self):
+        if self.preset_gender:
+            return "preset"
+        if self.custom_gender:
+            return "custom"
+        if self.variable:
+            return "variable"
+        if self.transition:
+            return "transition"
+        if self.discrepancy:
+            return "discrepancy"
+
     @property
     def active(self):
-        return self.preset_gender or self.custom_gender or self.variable or self.transition or self.discrepancy
+        return next((item for item in self.options if item is not None))
+
+
+class Length(Attribute):
+    letter = "L"
+
+    def __init__(self, vauge: Optional[str] = None, specific: Optional[Decimal] = None,
+                 arm: Optional[Decimal] = None, leg: Optional[Decimal] = None,
+                 neck: Optional[Decimal] = None, tail: Optional[Decimal] = None,
+                 wingspan: Optional[Decimal] = None):
+        self.vauge = vauge
+        self.specific = specific
+
+        self.arm = arm
+        self.leg = leg
+        self.neck = neck
+        self.tail = tail
+        self.wingspan = wingspan
+
+        if sum(a is not None for a in (self.vauge, self.specific)) != 1:
+            raise TypeError("Vauge and specific lengths are mutually exculsive.")
+
+
+class Width(Attribute):
+    letter = "W"
+
+    def __init__(self, value: Literal[StandardModifiers, "~"]):
+        self.value = value
+
+
+class Weight(Attribute):
+    letter = "T"
+
+    def __init__(self, vauge: Optional[str] = None, specific: Optional[Decimal] = None):
+        self.vauge = vauge
+        self.specific = specific
+
+        if sum(a is not None for a in (self.vauge, self.specific)) != 1:
+            raise TypeError("Vauge and specific lengths are mutually exculsive.")
 
 
 class DragonAttributes:
-    def __init__(self, gender = Optional[str]):
-        pass
+    def __init__(self,
+                 gender: Optional[Gender] = None,
+                 length: Optional[Length] = None,
+                 width: Optional[Width] = None,
+                 weight: Optional[Weight] = None):
+        self.gender = gender
+        self.length = length
+        self.width = width
+        self.weight = weight
 
     def __str__(self) -> str:
-        return "attr"
+        return "attrs"
 
 
 class Species:
@@ -46,17 +121,16 @@ class Species:
 
 class DragonCode:
     def __init__(self, species: Species, attributes: DragonAttributes,
-                 shapeshifter: bool, multispecies: Optional[List[Species]] = None,
+                 multispecies: Optional[List[Species]] = None,
                  shaped_species: Optional[Species] = None, alternate_form: Optional[Species] = None):
         self.species = species
         self.multispecies = multispecies
         self.shaped_species = shaped_species
         self.alternate_form = alternate_form
         self.attributes = attributes
-        self.shapeshifter = shapeshifter
 
     @classmethod
-    def parse(cls, code):
+    def parse(cls, code: str):
         if not code.startswith("DC2."):
             raise ValueError("Code does not begin with DC2.")
 
