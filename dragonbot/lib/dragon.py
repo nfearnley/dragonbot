994899f69fe882ad8d27f46431cf5a9fd2bdf408
DigiDuncan <digiduncan@gmail.com>
2021-05-19 00:17:04 -0400

    oh god here we go

--- dragonbot/lib/dragon.py
+++ dragonbot/lib/dragon.py
@@ -1,8 +1,33 @@
-from typing import List
+from typing import List, Literal, Optional, Tuple
+
+GenderPreset = Literal['f', 'h', 'm', 'n', 'p', '?']
+
+
+class Length:
+    def __init__(self) -> None:
+        pass
+
+
+class Gender:
+    def __init__(self, preset_gender: GenderPreset, custom_gender: str, variable: List[str],
+                 transition: Tuple[str, str], discrepancy: Tuple[str, str]) -> None:
+
+        if sum(a is not None for a in (preset_gender, custom_gender, variable, transition, discrepancy)) != 1:
+            raise TypeError("Didn't pass one type of gender in.")
+
+        self.preset_gender = preset_gender
+        self.custom_gender = custom_gender
+        self.variable = variable
+        self.transition = transition
+        self.discrepancy = discrepancy
+
+    @property
+    def active(self):
+        return self.preset_gender or self.custom_gender or self.variable or self.transition or self.discrepancy
 
 
 class DragonAttributes:
-    def __init__(self):
+    def __init__(self, gender = Optional[str]):
         pass
 
     def __str__(self) -> str:
@@ -10,8 +35,9 @@ class DragonAttributes:
 
 
 class Species:
-    def __init__(self, name: str, attributes: DragonAttributes = None):
+    def __init__(self, name: str, attributes: DragonAttributes = None, subtype: str = None):
         self.name = name
+        self.subtype = subtype
         self.attributes = attributes
 
     def __str__(self) -> str:
@@ -19,8 +45,13 @@ class Species:
 
 
 class DragonCode:
-    def __init__(self, species: List[Species], attributes: DragonAttributes, shapeshifter: bool):
+    def __init__(self, species: Species, attributes: DragonAttributes,
+                 shapeshifter: bool, multispecies: Optional[List[Species]] = None,
+                 shaped_species: Optional[Species] = None, alternate_form: Optional[Species] = None):
         self.species = species
+        self.multispecies = multispecies
+        self.shaped_species = shaped_species
+        self.alternate_form = alternate_form
         self.attributes = attributes
         self.shapeshifter = shapeshifter
 
